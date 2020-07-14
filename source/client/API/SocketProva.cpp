ff44c0f262b50911445f499388c174b6288c28fb
lorenzo ceccarelli <lorenzo.ceccarelli97@gmail.com>
2020-07-14 11:10:01 +0200

    prova

---
+++ source/client/API/SocketProva.cpp
@@ -0,0 +1,5 @@
+//
+// Created by loren on 14/07/2020.
+//
+
+#include "SocketProva.h"
---
+++ source/client/API/SocketProva.h
@@ -0,0 +1,14 @@
+//
+// Created by loren on 14/07/2020.
+//
+
+#ifndef PROGETTO_PDS_SOCKETPROVA_H
+#define PROGETTO_PDS_SOCKETPROVA_H
+
+
+class SocketProva {
+int lorenzoProva;
+};
+
+
+#endif //PROGETTO_PDS_SOCKETPROVA_H
