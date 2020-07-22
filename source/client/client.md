cb6583125283ad24dd0c037fbb8037648f056ad2
giandonatofarina <giandonato.farina@gmail.com>
2020-07-22 11:18:49 +0200

    Update client skeleton

--- source/client/client.md
+++ source/client/client.md
@@ -1,1 +1,5 @@
 # Client
+### Istruzioni pe l'uso
+* La cartella contiene solo il CMakeList.txt, quindi prima di cominciare ricarare il cmake (tasto destro -> Reload Cmake Project).
+* Nel CMake c'è un comando commentato in cui imposto la cartella di OpenSSL. Su macOS è necessario, su linux non so. In caso il CMake non funzioni copiate il path di OpenSSL in quel comando e togliete il commento.
+* Nel main ho scritto due test per controllare che Boost e OpenSSL funzionino.
--- source/client/main.cpp
+++ source/client/main.cpp
@@ -2,15 +2,25 @@
 // Created by bruno on 14/07/2020.
 //
 
-#include "ClientProva.h"
-#include "./API/SocketProvaClient.h"
+#include <iostream>
+#include <boost/rational.hpp>
+#include <openssl/sha.h>
 
 int main(){
 
-    ClientProva s;
-    SocketProvaClient s1;
+    std::cout<<"Client\n";
 
-    s.printInfo();
-    s1.printInfo();
+    // Boost test
+    boost::rational<int> a (2,3);
+    std::cout<<"Prova boost: " << a << std::endl;
 
+    // OpenSSL test
+    unsigned char ibuf[] = "sha1 test";
+    unsigned char obuf[20];
+
+    SHA1(ibuf, 10, obuf);
+    std::cout<<"SHA1: ";
+    for (int i = 0; i < 20; i++) printf("%02x ", obuf[i]);
+
+    return 0;
 }
