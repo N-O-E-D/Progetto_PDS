7de9f3b1a3d65fe71405f0f50ff7dca0fa042494
giandonatofarina <giandonato.farina@gmail.com>
2020-07-23 18:01:49 +0200

    Some changes in FileSystemWatcher

---
+++ source/client/FileSystemWatcher.h
@@ -0,0 +1,30 @@
+//
+// Created by Giandonato Farina on 23/07/2020.
+//
+
+#pragma once
+
+#include <string>
+#include <chrono>
+#include <filesystem>
+#include <unordered_map>
+#include <thread>
+
+// Define all possible changes
+enum class Status { FileCreated, FileModified, Erased, DirCreated};
+
+class FileSystemWatcher {
+public:
+    std::string path_to_watch;
+    std::chrono::duration<int, std::milli> delay;
+
+    FileSystemWatcher(const std::string& path_to_watch, std::chrono::duration<int, std::milli> delay);
+    void start(const std::function<void (std::string, Status)> &action);
+
+private:
+    std::unordered_map<std::string, std::filesystem::file_time_type> paths_;
+    bool running_ = true;
+
+    bool contains(const std::string &key);
+};
+
--- source/client/client.md
+++ source/client/client.md
@@ -7,10 +7,9 @@
 * Due thread: uno per il FileWatcher e uno per comunicazione con server.
 * Modifica del FileWatcher con inserimento dei seguenti stati:
     * FileCreated
-    * FileErased
+    * Erased
     * FileModified
     * DirCreated
-    * DirModified
 * Per ogni modifica rilevata inserisco in una coda path e status e risveglio il thread di comunicazione
 * Thread di comunicazione apre un socket e legge la coda: in base allo status chiama la corrispettiva funzione per inviare un messaggio al server.
 * Provare ad implementare gli status FileRenamed e DirRenamed 
