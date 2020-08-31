bc5c86b226a8f39a13513d6ac320583354bdd5eb
bruno <bruno.accogli@libero.it>
2020-08-31 17:45:11 +0200

    Cookies - CookieMap class created

--- source/server/main.cpp
+++ source/server/main.cpp
@@ -1,5 +1,6 @@
-
 #include "../../CryptoFunctions/CryptoExecutor.h"
+#include "../API_server/ServerSocket.h"
+#include "./Server.h"
 #include <ctime>
 #include <iostream>
 #include <string>
@@ -13,14 +14,6 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/iterator.hpp>
 #include <boost/cstdint.hpp>
-#include "./Server.h"
-#include "../API_server/ServerSocket.h"
-
-//create a Server object (provides ADD, MODIFY, REMOVE methods)
-Server server;
-
-std::vector<char> bufferprova = {'c','i','a','o'};
-ssize_t buffsize = bufferprova.size();
 
 std::map<std::string,int> commands = {
         {"UPDATE",1},
@@ -46,36 +39,9 @@ int selectComando(std::string& str, std::string& path, std::string& newpath){
     std::cout<<cmd<<" "<<path<<" "<<newpath<<std::endl;
     return commands[cmd];
 }
-/*
-bool autentica(boost::asio::streambuf& recmessage){
-    //from streambuf to string
-    std::string recmex(boost::asio::buffers_begin(recmessage.data()),boost::asio::buffers_begin(recmessage.data())+recmessage.size());
-    auto username = recmex.substr(0,recmex.find(" "));
-    auto password = recmex.substr(username.length()+1);
-    std::cout<<username<<std::endl;
-    password.pop_back();  //rimuove "\n" (next line)
-    password.pop_back();  //rimuove "\r" (carriage return)
-    std::cout<<password<<std::endl;
-    std::cout<<users.at(username)<<std::endl;
-    try{
-        if(password.compare(users.at(username))==0) {
-            std::cout<<"Benvenuto, "<<username<<"!"<<std::endl;
-            return true;
-        }
-        else {
-            std::cout<<"Password non corretta."<<std::endl;
-            return false;
-        }
-    }
-    catch(std::out_of_range& exception){
-        std::cout<<"eccezione"<<std::endl;
-        return false;}
-
-}
-*/
-
 void handleSocket(int portnum){
 
+/*
     Server s;
     //s.setUserDirectory("Bruno");
     //s.updateName("/home/roba.txt","/home/roba2.txt");
@@ -83,6 +49,7 @@ void handleSocket(int portnum){
     //s.removeDir("/acaso/cart");
     //s.createDir("/home/nuova");
     //s.syncDir("/home/nuova");
+*/
 
     //while(true){
     try {
@@ -95,101 +62,6 @@ void handleSocket(int portnum){
     }
     //}
 
-
-    /*
-    try
-    {
-        // Any program that uses asio need to have at least one io_service object
-        boost::asio::io_service io_service;
-        //std::cout<<"ioservice created"<<std::endl;
-
-        // acceptor object needs to be created to listen for new connections
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), portnum));
-        //std::cout<<"acceptor created"<<std::endl;
-
-        while (true)
-        {
-            // creates a socket
-            tcp::socket socket(io_service);
-            //std::cout<<"socket created"<<std::endl;
-
-            // wait and listen
-            std::cout<<"Waiting for connection..."<<std::endl;
-            acceptor.accept(socket);
-
-            // prepare message to send back to client
-            auto sendmessage = boost::asio::buffer("Hello from server!\nPlease authenticate (username password):");
-
-            //boost::system::error_code ignored_error;
-
-            // writing the message
-            //socket.write_some(sendmessage, ignored_error);
-            boost::asio::write(socket,boost::asio::buffer(sendmessage));
-
-
-
-            //prepare buf to store received message by client
-            boost::asio::streambuf recmessage;
-
-            //receive something on the socket
-            //int size = socket.read_some(recmessage,ignored_error);
-            //socket.write_some(recmessage, ignored_error);
-            boost::asio::read_until(socket,recmessage,"\n");
-
-            if(autentica(recmessage)) {
-                recmessage.consume(recmessage.size());  //pulisce il buffer in ricezione...
-                boost::asio::read_until(socket,recmessage,"\n");  //...per riempirlo subito dopo
-
-                //from streambuf to string
-                std::string recmex(boost::asio::buffers_begin(recmessage.data()),boost::asio::buffers_begin(recmessage.data())+recmessage.size());
-                std::string path;
-                std::string newpath;
-
-                //hash di un file a caso
-                unsigned char md_value[EVP_MAX_MD_SIZE];
-                unsigned int md_len = computeHash("../CMakeLists.txt", md_value);
-
-                switch (selectComando(recmex,path,newpath)) {
-                    case 1:
-                        server.update(path, bufferprova, buffsize);  //ok
-                        break;
-                    case 2:
-                        server.updateName(path,newpath);  //ok
-                        break;
-                    case 3:
-                        server.remove(path);  //ok
-                        break;
-                    case 4:
-                        server.removeDir(path);  //ok
-                        break;
-                    case 5:
-                        server.createFile(path, bufferprova, buffsize);  //ok
-                        break;
-                    case 6:
-                        server.createDir(path); //ok
-                        break;
-                    case 7:
-                        server.syncDir(path);  //ok
-                        break;
-                    case 8:
-                        server.syncFile(path,md_value,md_len);
-                        break;
-                    default:
-                        std::cout << "Comando errato" << std::endl;
-                        break;
-                }
-
-            }
-
-            //boost::asio::write(socket,recmessage);
-        }
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    return;
-     */
 }
 
 
@@ -206,13 +78,15 @@ int main()
     //launch the pool with num_threads threads
     //Ogni thread gestisce un client (su una porta diversa), tramite una coda di richieste provenienti dallo stesso
     //si possono gestire fino a num_threads clients contemporaneamente
-    boost::asio::thread_pool pool(4); //una coda di thread di dimensione 4: 4 thread alla volta possono essere eseguiti in parallelo
+    //boost::asio::thread_pool pool(4); //una coda di thread di dimensione 4: 4 thread alla volta possono essere eseguiti in parallelo
 
     //submit a function to the pool
-    boost::asio::post(pool,[](){handleSocket(5000);});
+    //boost::asio::post(pool,[](){handleSocket(5000);});
     //boost::asio::post(pool,[](){handleSocket(5001);});
 
-    pool.join();
+    handleSocket(5000);
+
+    //pool.join();
 
 /*
     const std::string filename("../credenziali.txt");
@@ -227,16 +101,15 @@ int main()
     std::string passw;
     s.UserToPassword(user,passw);
     std::cout<<passw<<std::endl;
-
-    return 0;
 */
 
-    /*BIGNUM *prova = BN_new();
+/*
+    BIGNUM *prova = BN_new();
     Cookie c;
     std::this_thread::sleep_for(std::chrono::seconds(2));
     Cookie c2(prova);
-    std::cout<<c.is_expired()<<std::endl;*/
+    std::cout<<c.is_expired()<<std::endl;
+*/
 
     return 0;
-
 }
